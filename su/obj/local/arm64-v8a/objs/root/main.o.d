d50920464c26898519c2bd45f564b08c6d366395
chicwalkiog <rizixi@foxmail.com>
2022-11-10 14:54:13 +0800

    删除无用的文件

--- su/obj/local/arm64-v8a/objs/root/main.o.d
+++
@@ -1,8 +0,0 @@
-/home/chic/android-jailbreak/su/obj/local/arm64-v8a/objs/root/main.o: \
-  /home/chic/android-jailbreak/su/jni/main.cpp \
-  /home/chic/android-jailbreak/su/jni/su_service.h \
-  /home/chic/android-jailbreak/su/jni/su_client.h
-
-/home/chic/android-jailbreak/su/jni/su_service.h:
-
-/home/chic/android-jailbreak/su/jni/su_client.h:
--- su/obj/local/arm64-v8a/objs/su/pts.o.d
+++
@@ -1,8 +0,0 @@
-/home/chic/android-jailbreak/su/obj/local/arm64-v8a/objs/su/pts.o: \
-  /home/chic/android-jailbreak/su/jni/pts.cpp \
-  /home/chic/android-jailbreak/su/jni/pts.h \
-  /home/chic/android-jailbreak/su/jni/tool.h
-
-/home/chic/android-jailbreak/su/jni/pts.h:
-
-/home/chic/android-jailbreak/su/jni/tool.h:
